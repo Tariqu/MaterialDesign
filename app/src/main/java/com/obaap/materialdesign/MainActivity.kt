6b6ffe045dc653521475a0a2ad77c3922d5e7f11
Tarique Akhtar Ansari <tarique.rkl@gmail.com>
2018-07-03 17:54:57 +0530

    Google sign with logout option from menu implemented with visible/inVisible sign_out option

--- app/src/main/java/com/obaap/materialdesign/MainActivity.kt
+++ app/src/main/java/com/obaap/materialdesign/MainActivity.kt
@@ -2,59 +2,109 @@ package com.obaap.materialdesign
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-
-
-
+import android.content.Intent
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    lateinit var gso:GoogleSignInOptions
-    private var mGoogleSignInClient: GoogleSignInClient? = null
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        title="Login Page"
-        bt_google.setOnClickListener {
-            gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.default_web_client_id))
-                    .requestEmail()
-                    .build()
 
-        }
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-    }
-
+    lateinit var gso: GoogleSignInOptions
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val RC_SIGN_IN: Int = 1
+    lateinit var signOut: Button
+    lateinit var mAuth: FirebaseAuth
+    var menu:Menu?=null
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        var menuInflator =menuInflater
-        menuInflator.inflate(R.menu.main_menu,menu)
-
+        var inflater = menuInflater
+        inflater.inflate(R.menu.main_menu, menu)
+        this.menu=menu
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item!!.itemId){
-            R.id.New-> Toast.makeText(this@MainActivity,item!!.title,Toast.LENGTH_SHORT).show()
-            R.id.about->Toast.makeText(this@MainActivity,item!!.title,Toast.LENGTH_SHORT).show()
-            R.id.Open->Toast.makeText(this@MainActivity,item!!.title,Toast.LENGTH_SHORT).show()
-            R.id.tarique->Toast.makeText(this@MainActivity,item!!.title,Toast.LENGTH_SHORT).show()
+        when (item!!.itemId) {
+            R.id.tarique -> {
+                Toast.makeText(this@MainActivity, item!!.title, Toast.LENGTH_SHORT).show()
+                title = item!!.title
+            }
+
+            R.id.akhtar -> {
+                Toast.makeText(this@MainActivity, item!!.title, Toast.LENGTH_SHORT).show()
+                title = item!!.title
+            }
+            R.id.sign_out -> {
+                Toast.makeText(this@MainActivity, item!!.title, Toast.LENGTH_SHORT).show()
+                title = item!!.title
+                mGoogleSignInClient.signOut().addOnCompleteListener { task: Task<Void> ->
+                    if (task.isSuccessful) {
+                        dispTxt.text = " "
+                        item!!.isVisible = false
+                    }
+                }
+            }
+            R.id.about -> {
+                Toast.makeText(this@MainActivity, item!!.title, Toast.LENGTH_SHORT).show()
+                title = item!!.title
+            }
         }
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
+        val signIn = findViewById<View>(R.id.signInBtn) as SignInButton
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        signIn.setOnClickListener { view: View? ->
+            signIn()
+        }
+    }
 
+    private fun signIn() {
+        val signInIntent: Intent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+        } else {
+            Toast.makeText(this, "Problem in execution order :(", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account: GoogleSignInAccount = completedTask.getResult(ApiException::class.java)
+            updateUI(account)
+        } catch (e: ApiException) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        dispTxt.text = account.displayName + "\n" + account.email + "\n" + account.familyName
+        var sign_out =menu!!.findItem(R.id.sign_out)
+        sign_out.isVisible=true
+        title=account.displayName
+    }
 }
